dd685fdf20a307d652820face1608f34ec532ddd
im-sudhir <ssudhir794@gmail.com>
2025-05-29 19:41:37 +0530

    used in built login views

--- account/views.py
+++ account/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -25,4 +26,12 @@ def user_login(request):
     else:
         form=LoginForm()
     return render(request,'account/login.html', {'form':form})
-                         
+
+
+@login_required
+def dashboard(request):
+    return render(
+        request,
+        'account/dashboard.html',
+        {'section':'dashboard'}
+    )
